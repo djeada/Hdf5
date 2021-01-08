e0a39ec5053bb4217dfabe51fa02da700403f637
djeada <addjellouli1@gmail.com>
2021-01-08 16:29:29 +0100

    divided the file into smaller functions

--- src/basic_io.py
+++ src/basic_io.py
@@ -4,23 +4,32 @@ import random
 
 path = 'example_hdf5.h5'
 
-data = [ np.random.random(size = (random.randint(1,1000), random.randint(1,1000))) for i in range(3)]
+def random_data():
+	return [np.random.random(size = (random.randint(1,1000), random.randint(1,1000))) for i in range(3)]
 
-with h5py.File(path, 'w') as hdf:
-	for matrix in data:
-		hdf.create_dataset('dataset {}'.format(data.index(matrix) + 1), data=matrix)
+def save_data(_path, data):
+	with h5py.File(_path, 'w') as hdf:
+		for matrix in data:
+			hdf.create_dataset('dataset {}'.format(data.index(matrix) + 1), data=matrix)
 
-with h5py.File(path,'r') as hdf:
-	ls = list(hdf.keys())
-	print('List of datasets in this file: \n', ls)
+def read_data(_path):
+	with h5py.File(_path,'r') as hdf:
+		ls = list(hdf.keys())
+		print('List of datasets in this file: \n', ls)
 
-	for key in ls:
-		print('')
-		data = hdf.get(key)
-		print(data)
-		dataset = np.array(data)
-		print('Shape of {} : {}'.format(key, dataset.shape))
-		print('Contents of the dataset')
-		print(dataset)
+		for key in ls:
+			print('')
+			data = hdf.get(key)
+			print(data)
+			dataset = np.array(data)
+			print('Shape of {} : {}'.format(key, dataset.shape))
+			print('Contents of the dataset')
+			print(dataset)
 
+def main():
+	data = random_data()
+	save_data(path, data)
+	read_data(path)
 
+if __name__ == "__main__":
+	main()

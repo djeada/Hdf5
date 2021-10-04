85578526f131119d6b8142815859d0fc360848e0
djeada <addjellouli1@gmail.com>
2021-10-04 22:43:18 +0200

    correcting groups examples, formatting

--- src/groups.py
+++ src/groups.py
@@ -1,37 +1,48 @@
 import numpy as np
 import h5py
-import random
 
-path = 'example_hdf5.h5'
+PATH = "example_groups.h5"
+NUM_DATASETS = 3
+NUM_ROWS = 100
+NUM_COLS = 10
+
 
 def random_data():
-	return [np.random.random(size = (random.randint(1,1000), random.randint(1,1000))) for i in range(3)]
+    return [np.random.random(size=(NUM_ROWS, NUM_COLS)) for i in range(NUM_DATASETS)]
+
 
 def save_data(_path, data):
-	with h5py.File(_path, 'w') as hdf:
-		G1 = hdf.create_group('Group1')
-		
-		subgroups = [G1.create_group(name) for name in ['Group1/SubGroup1', 'Group1/SubGroup2']]
+    with h5py.File(_path, "w") as hdf:
+        group = hdf.create_group("Group1")
 
-		for subgroup in subgroups:
-			for matrix in data:
-				subgroup.create_dataset('dataset {}'.format(data.index(matrix) + 1), data=matrix)
+        subgroups = [
+            group.create_group(name)
+            for name in ["Group1/SubGroup1", "Group1/SubGroup2"]
+        ]
 
-def read_data(_path):
-	with h5py.File(_path,'r') as hdf:
-		base_items = list(hdf.items())
-		print('Items in the base directory:', base_items)
-		ls = list(hdf.keys())
-		print('List of datasets in this file: \n', ls)
+        for subgroup in subgroups:
+            for i, matrix in enumerate(data):
+                subgroup.create_dataset(f"dataset {i}", data=matrix)
 
-		for key in ls:
-			for item in hdf.get(key).items():
-				print(item[1].keys())
+
+def read_data(path):
+    with h5py.File(path, "r") as hdf:
+        base_items = list(hdf.items())
+        print(f"Items in the base directory: {base_items}")
+
+        dataset_list = list(hdf.keys())
+        print(f"List of datasets in this file: \n {dataset_list}")
+
+        for key in dataset_list:
+            for item in hdf.get(key).items():
+                print(list(item[1].keys()))
+
 
 def main():
-	data = random_data()
-	save_data(path, data)
-	read_data(path)
+    data = random_data()
+    save_data(PATH, data)
+    read_data(PATH)
+
 
 if __name__ == "__main__":
-	main()
+    main()
